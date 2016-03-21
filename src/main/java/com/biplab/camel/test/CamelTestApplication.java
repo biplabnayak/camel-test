ee6127361a5f5e08998eb085fa931857992a4493
biplabnayak <prasad_biplab@yahoo.com>
2016-03-21 16:42:05 +0530

    initial setup

---
+++ src/main/java/com/biplab/camel/test/CamelTestApplication.java
@@ -0,0 +1,12 @@
+package com.biplab.camel.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelTestApplication.class, args);
+	}
+}
